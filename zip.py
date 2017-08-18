9ab6bfc12dec1c4a44c2250a539d24ed9d4da4b6
xiaoming <1746534101@qq.com>
2017-08-18 18:34:34 +0800

    ii

---
+++ zip.py
@@ -0,0 +1,21 @@
+import os
+import zipfile
+
+def zipdir(path, ziph,id):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            print(root, dirs, files)
+            if file=='app.js':
+                with open(root+file, 'r') as myfile:
+                    data = myfile.read().replace('REQUEST_MERCHANT_VALUE', str(id))
+                    print(data)
+
+            ziph.write(os.path.join(root, file))
+
+
+if __name__ == '__main__':
+    for i in range(1, 3):
+        zipf = zipfile.ZipFile('test'+str(i)+'.zip', 'w', zipfile.ZIP_DEFLATED)
+        zipdir('dist/', zipf,i)
+        zipf.close()
